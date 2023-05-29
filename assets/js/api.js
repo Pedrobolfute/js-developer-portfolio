7e5d97115b346f2501a609cbcce2518bb410fff5
Pedrobolfute <pepenike-101@hotmail.com>
2023-05-29 20:30:25 -0300

    changed link from api request. main to pedro branch

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function requestJsonInfo(){
-  const url = 'https://raw.githubusercontent.com/Pedrobolfute/js-developer-portfolio/main/data/profile.json'
+  const url = 'https://raw.githubusercontent.com/Pedrobolfute/js-developer-portfolio/pedro/data/profile.json'
   const responseInfo = await fetch(url)
   return responseInfo.json()
 }
