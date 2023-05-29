9add00f055ef3ff456d9b6df5376f160adaf6278
Pedrobolfute <pepenike-101@hotmail.com>
2023-05-29 15:42:44 -0300

    potifolio added

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,27 @@ function updateProfileInfo(profileData){
 function updateProfileSkills(profileData){
   const profileHardSkills = document.querySelector('.tools ul')
   profileHardSkills.innerHTML = profileData.skills.hardSkills.map(skill =>`
-    <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>
-    `).join('')
+  <li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>
+  `).join('')
 
   const profileSoftSkills = document.querySelector('.personal ul')
   profileSoftSkills.innerHTML = profileData.skills.softSkills.map(skill =>`
   <li>${skill}</li>`).join('')
+}
+
+function updateProfileLanguage(profileData){
+  const profileLanguage = document.querySelector('.languages')
+  profileLanguage.innerHTML = profileData.languages.map(language => `
+  <li>${language.name} (${language.level})</li>`).join('')
+}
+
+function updateProfilePortfolio(profileData){
+  const profilePortfolio = document.querySelector('.portfolio')
+  profilePortfolio.innerHTML = profileData.portfolio.map(function(data){
+   return `<li>
+    <h3 class="title ${data.gitHub ? 'github' : ''}">${data.name}</h3>
+    <a href="${data.gitHub}" target="_blank">${data.gitHub}</a>
+    </li>`}).join('')
 }
 
 (async () => {
@@ -38,4 +53,6 @@ function updateProfileSkills(profileData){
 
   updateProfileInfo(profileData)
   updateProfileSkills(profileData)
+  updateProfileLanguage(profileData)
+  updateProfilePortfolio(profileData)
 })()
