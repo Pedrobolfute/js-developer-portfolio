e7c5dd23a17a2007e0e9a2af4178db2314becb0c
Pedrobolfute <pepenike-101@hotmail.com>
2024-03-10 19:50:53 -0300

    feat: acordeon Cursos e Bootcamps added, and some svgs fixes

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,3 +3,6 @@ async function requestJsonInfo(){
   const responseInfo = await fetch(url)
   return responseInfo.json()
 }
+
+// const url = 'https://raw.githubusercontent.com/Pedrobolfute/js-developer-portfolio/pedro/data/profile.json'
+// const url = '../data/profile.json'
